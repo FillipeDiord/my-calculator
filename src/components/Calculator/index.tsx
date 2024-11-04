8067b8e8e2eb15298122d8cc647011a49a687d59
Fillipe Diord <fillipe.diord@hotmail.com>
2024-11-03 21:30:16 -0300

    feat: 🎸 adding new styles for Calculator component
    adding new styles for Calculator component

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import { Box, Button, Grid2, TextField } from "@mui/material";
+import { Box, Button, Grid2, InputBase } from "@mui/material";
 import { evaluate, sqrt } from "mathjs";
 
 export const Calculator: React.FC = () => {
   const [input, setInput] = useState<string>("");
 
   const handleClick = (value: string) => {
-    if (value === '√') {
+    if (value === "√") {
       try {
         setInput(sqrt(parseFloat(input)).toString());
       } catch {
-        setInput('Error');
+        setInput("Error");
       }
     } else {
       setInput(input + value);
@@ -34,11 +34,36 @@ export const Calculator: React.FC = () => {
   };
 
   return (
-    <Box>
-      <TextField value={input} disabled fullWidth variant="outlined" />
+    <Box
+      sx={{
+        minWidth: 250,
+        maxWidth: 400,
+        bgcolor: "#b7b8b9",
+        border: "2px solid #0a0a0a",
+        borderRadius: 1,
+        padding: 2,
+      }}
+    >
+      <InputBase
+        sx={{
+          bgcolor: "#FFFFFF",
+          border: "2px solid #0a0a0a",
+          borderRadius: 2,
+          padding: 1,
+          height: 60,
+          color: "#033261",
+          pointerEvents: "none",
+          userSelect: "none",
+          fontWeight: 600,
+        }}
+        value={input}
+        inputProps={{ style: { textAlign: "center" } }}
+        fullWidth
+        defaultValue={0}
+      />
       <Grid2 container spacing={1} margin={2}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
-          <Grid2 size={3} key={num}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+          <Grid2 size={4} key={num}>
             <Button
               variant="contained"
               onClick={() => handleClick(num.toString())}
@@ -50,36 +75,49 @@ export const Calculator: React.FC = () => {
       </Grid2>
       <Grid2
         container
-        spacing={2}
+        size={2}
         alignContent="center"
         justifyContent="center"
-        flexDirection="column"
+        flexDirection="row"
       >
-        <Grid2 display="flex" gap={1}>
-          <Button variant="contained" onClick={() => handleClick("+")}>
-            +
-          </Button>
-          <Button variant="contained" onClick={() => handleClick("-")}>
-            -
-          </Button>
+        <Grid2 display="flex" gap={6} margin={2}>
+          <Grid2>
+            <Button variant="contained" onClick={() => handleClick("0")}>
+              0
+            </Button>
+          </Grid2>
+          <Grid2>
+            <Button variant="contained" onClick={() => handleClick("+")}>
+              +
+            </Button>
+          </Grid2>
+          <Grid2>
+            <Button variant="contained" onClick={() => handleClick("-")}>
+              -
+            </Button>
+          </Grid2>
         </Grid2>
-        <Grid2 display="flex" gap={1}>
-          <Button variant="contained" onClick={() => handleClick("/")}>
-            /
-          </Button>
-          <Button variant="contained" onClick={() => handleClick("√")}>
-            √
-          </Button>
-        </Grid2>
-        <Grid2 display="flex" gap={1}>
-          <Button variant="contained" onClick={handleEqual}>
-            =
-          </Button>
-          <Button variant="contained" onClick={handleClear}>
-            CE
-          </Button>
+        <Grid2 display="flex" gap={6} margin={2}>
+          <Grid2>
+            <Button variant="contained" onClick={() => handleClick("/")}>
+              /
+            </Button>
+          </Grid2>
+          <Grid2>
+            <Button variant="contained" onClick={() => handleClick("√")}>
+              √
+            </Button>
+          </Grid2>
+          <Grid2>
+            <Button variant="contained" onClick={handleEqual}>
+              =
+            </Button>
+          </Grid2>
         </Grid2>
       </Grid2>
+      <Button variant="contained" onClick={handleClear}>
+        CE
+      </Button>
     </Box>
   );
 };
